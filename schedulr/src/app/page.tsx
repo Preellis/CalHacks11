174e6178645b239566c3c1e996fafb624dcc8864
Preston Ellis <prestone03@gmail.com>
2024-10-19 11:33:52 -0700

    Update page.tsx

--- schedulr/src/app/page.tsx
+++ schedulr/src/app/page.tsx
@@ -65,12 +65,9 @@ export default function Home() {
 
   return (
     <div>
-<<<<<<< HEAD
-=======
       <p>Hello World</p>
       <TestGemini />
       <TestDeepgram />
->>>>>>> eadfc043b936952f09526c1e47c9da719c4da7c2
       {user ? (
         <div>Logged in!</div>
       ) : (
