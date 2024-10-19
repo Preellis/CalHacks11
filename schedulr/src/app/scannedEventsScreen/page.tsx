19fd598d4a6dac5e41cc600ce3ed5587979ea121
Edwardcg17 <edwche577@berkeley.edu>
2024-10-19 15:59:30 -0700

    image to gemini not working

--- schedulr/src/app/scannedEventsScreen/page.tsx
+++ schedulr/src/app/scannedEventsScreen/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
-import { useAtom } from 'jotai';
-import { imageAtom } from '@/atoms';
+
 import React, { useState } from "react";
 import axios from "axios";
 
 
 const ScannedEventsScreen: React.FC = () => {
-    const [image, setImage] = useAtom(imageAtom);
     const [result, setResult] = useState<string | null>(null);
 
     const promptGemini = async () => {
