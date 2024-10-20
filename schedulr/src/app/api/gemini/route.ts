ac02b14cc0af89a0408aef01e424f89031eeca1b
Preston Ellis <prestone03@gmail.com>
2024-10-19 18:38:03 -0700

    add event enhancer

--- schedulr/src/app/api/gemini/route.ts
+++ schedulr/src/app/api/gemini/route.ts
@@ -7,23 +7,9 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
   try {
-    const { base64Image } = await request.json();
-
-    const prompt =
-      "Parse this image into a .json file. Return the .json file only, do not return any other text. The .json file should be in the format of 'eventname', 'date', 'time', 'location'. If any values are not found, return null for that category in .json.";
-
-    const response = await model.generateContent([
-      prompt,
-      {
-        inlineData: {
-          data: base64Image,
-          mimeType: "image/png",
-        },
-      },
-    ]);
-
+    const { prompt } = await request.json();
+    const response = await model.generateContent([prompt]);
     const result = response.response.text();
-
     return NextResponse.json({ response: result });
   } catch (error) {
     console.error("Error generating content:", error);
