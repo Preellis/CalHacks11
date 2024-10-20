06bb222e63a06e62fc3dbdda091d4b056ba4ec63
Preston Ellis <prestone03@gmail.com>
2024-10-19 17:13:29 -0700

    fix gemini

--- schedulr/src/app/api/gemini/route.ts
+++ schedulr/src/app/api/gemini/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Storage } from "@google-cloud/storage";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { decode } from "base64-arraybuffer";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   const apiKey = process.env.GEMINI_API!;
@@ -11,29 +9,6 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
   try {
     const { base64Image } = await request.json();
 
-    // // Initialize Google Cloud Storage
-    // const storage = new Storage({
-    //   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
-    //   credentials: JSON.parse(process.env.GOOGLE_CLOUD_CREDENTIALS!)
-    // });
-
-    // const bucketName = process.env.GOOGLE_CLOUD_BUCKET_NAME!;
-    // const bucket = storage.bucket(bucketName);
-
-    // // Generate a unique filename
-    // const filename = `event_image_${Date.now()}.jpg`;
-    // const file = bucket.file(filename);
-
-    // // Upload the base64 image to Google Cloud Storage
-    // await file.save(Buffer.from(decode(base64Image)), {
-    //   metadata: {
-    //     contentType: 'image/jpeg',
-    //   },
-    // });
-
-    // // Get the public URL of the uploaded file
-    // const publicUrl = `https://storage.googleapis.com/${bucketName}/${filename}`;
-
     const prompt =
       "Parse this image into a .json file. Return the .json file only, do not return any other text. The .json file should be in the format of 'eventname', 'date', 'time', 'location'. If any values are not found, return null for that category in .json.";
 
@@ -42,7 +17,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
       {
         inlineData: {
           data: base64Image,
-          mimeType: "image/jpeg",
+          mimeType: "image/png",
         },
       },
     ]);
